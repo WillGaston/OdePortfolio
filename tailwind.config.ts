120a11eeb459750c5f434ad5088c4f9d45f3c7b2
William Gaston <z5591798@ad.unsw.edu.au>
2025-12-07 11:18:43 +1100

    vite setup

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  theme: {
+    fontFamily: {
+      serif: ["Playfair Display", "ui-serif", "Georgia"],
+    },
+  },
+} satisfies Config;
